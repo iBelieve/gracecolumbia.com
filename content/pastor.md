21e21b0ca1a77087f740fbb7f700709377743549
Michael Spencer <sonrisesoftware@gmail.com>
2017-07-22 10:02:09 -0500

    Initial project

---
+++ content/pastor.md
@@ -0,0 +1,19 @@
+---
+title: Our Pastor
+---
+
+Pastor Micah A. Ernst was born August 17, 1957 to August and Mildred Ernst, in Buffalo, Minnesota. He is the fourth child of seven; two sisters and 4 brothers. He attended Salem Lutheran Christian Day School at Loretto, Minnesota, to the 4th grade and finished his education at the public schools in Buffalo, Minnesota. After high school he worked in construction for 5 years. On June 24th, 1979, Micah and Claudia, his high school sweetheart, were united in Holy Matrimony. Claudia, a graduate of Dr. Martin Luther College of New Ulm, Minnesota, taught school at Trinity Lutheran, Belle Plaine, Minnesota. Through much heartfelt prodding, the Lord truly directed Micah to continue his education to study for the pastoral ministry.
+
+Pastor Ernst received his college education at Bethany Lutheran College, Mankato, Minnesota. He then went to Wisconsin Lutheran Seminary, Mequon, Wisconsin, (WELS) for one year of seminary training. He finished his pastoral studies at Bethany Lutheran Theological Seminary, Mankato, Minnesota (ELS). During those years Micah and Claudia were blessed with 4 children (Nathanael, Hannah, Ezekiel, and Sarah).
+
+Pastor Ernst vicared in northern Iowa in a five-point parish under the direction of Pastor Alf Merseth. In May of 1988, he received the call to Peace Evangelical Lutheran Church (ELS) in Jefferson City, MO. In 1996 he received a call to also serve Grace Lutheran (WELS) in Columbia, MO. During these years, Pastor and Claudia were blessed with 4 more children (Christina, Joshua, Rachel, and Uriah).
+
+<div class="pull-left-highlight">
+In 1999, the Lord led Pastor and Claudia to adopt their handicapped great nephew, Cody. The Lord has blessed the Ernst family in many wonderful ways!
+</div>
+
+In 1999 he also accepted the call to Peace Lutheran in Deshler, Ohio. He served there for three years. Pastor, Claudia and family returned to Missouri. He then received the call to once again serve Grace Lutheran (WELS) in Columbia, MO, which he is presently serving.
+
+Led by the Lord to help provide for the spiritual needs of the people living at the Lake of the Ozarks area, he has started a preaching station in Osage Beach, MO named Beautiful Savior of the Lake. Here, he conducts services on Saturday evenings and Bible Class on Wednesday evenings.
+
+His love for the Lord’s peace and forgiveness leads him to reach out to others. Our Savior’s grace, continues to motivate his ministry.
---
+++ gulpfile.js
@@ -0,0 +1,169 @@
+const path     = require('path')
+const gulp     = require('gulp')
+const gutil    = require('gulp-util')
+const es       = require('event-stream')
+const plugins  = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*'],
+  replaceString: /\bgulp[\-.]/
+})
+const nunjucks = require('nunjucks')
+
+/***** METALSMITH SETUP *****/
+
+const metalsmith = {
+  layouts:     require('metalsmith-layouts'),
+  markdown:    require('metalsmith-markdown'),
+  permalinks:  require('metalsmith-permalinks')
+}
+
+const njk = nunjucks.configure('.', {
+  watch: false,
+  noCache : true
+})
+
+/***** GLOBAL CONSTANTS *****/
+
+const IS_DEV = process.env.NODE_ENV !== 'production'
+const SASS_STYLE = IS_DEV ? 'expanded' : 'compressed'
+const SOURCE_MAPS = !IS_DEV
+
+const METADATA = {
+  site: {
+    title: 'Grace Lutheran Church',
+    url: 'http://gracecolumbia.com'
+  },
+  google_analytics: '',
+  // description: 'It\'s about saying »Hello« to the world.',
+  links: {
+
+  }
+}
+
+const PATHS = {
+  content: {
+    files: 'content/**',
+    dest: './dist'
+  },
+  layouts: {
+    files: 'layouts/**'
+  },
+  partials: {
+    files: 'partials/**'
+  },
+  styles: {
+    sass: {
+      src: 'assets/sass',
+      files: 'assets/sass/**/*.scss',
+    },
+    css: {
+      src: 'assets/css',
+      files: 'assets/css/**/*.css'
+    },
+    dest: './dist/css/'
+  },
+  scripts: {
+    src: 'assets/js/**/*.js',
+    dest: './dist/js/'
+  },
+  images: {
+    src: 'assets/images/**/*',
+    dest: './dist/images/'
+  }
+}
+
+function changeEvent(prefix) {
+    const ACTIONS = {
+        'add': 'added',
+        'addDir': 'added',
+        'change': 'changed',
+        'unlink': 'deleted',
+        'unlinkDir': 'deleted'
+    }
+
+    return (evt, file) => {
+        gutil.log(gutil.colors.cyan(prefix), 'file', gutil.colors.green(path.relative(__dirname, file)), 'was', gutil.colors.magenta(ACTIONS[evt]));
+    }
+}
+
+/***** TASKS *****/
+
+function content() {
+  return gulp.src(PATHS.content.files)
+    .pipe(plugins.metalsmith({
+      root: __dirname,
+      use: [
+        metalsmith.markdown(),
+        metalsmith.layouts({
+          engine: 'nunjucks',
+          requires: { njk },
+          partials: 'partials',
+          default: 'default.njk',
+          cache: false
+        }),
+        metalsmith.permalinks({
+          relative: false
+        })
+      ],
+      metadata: METADATA
+    }))
+    .pipe(gulp.dest(PATHS.content.dest))
+}
+
+
+function styles() {
+  const sass = gulp.src(PATHS.styles.sass.files)
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass({
+      outputStyle: SASS_STYLE,
+      includePaths: [PATHS.styles.sass.src]
+    }))
+    .on('error', function(err) {
+      throw new gutil.PluginError('CSS', err, { showStack: true })
+    })
+
+  return es.concat(gulp.src(PATHS.styles.css.files), sass)
+    .pipe(plugins.concat('styles.css'))
+    .pipe(plugins.autoprefixer('last 2 versions'))
+    .pipe(IS_DEV ? gutil.noop() : plugins.cssmin())
+    .pipe(IS_DEV ? plugins.sourcemaps.write() : gutil.noop())
+    .pipe(plugins.size({ title: 'Styles' }))
+    .pipe(gulp.dest(PATHS.styles.dest))
+}
+
+function scripts() {
+  return gulp.src(PATHS.scripts.src)
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.babel())
+    .pipe(plugins.concat('scripts.js'))
+    .pipe(IS_DEV ? gutil.noop() : plugins.uglify())
+    .pipe(IS_DEV ? plugins.sourcemaps.write() : gutil.noop())
+    .pipe(plugins.size({ title: 'Scripts' }))
+    .pipe(gulp.dest(PATHS.scripts.dest))
+}
+
+function images() {
+  return gulp.src(PATHS.images.src, { since: gulp.lastRun(images) })
+    .pipe(plugins.size({ title: 'Images' }))
+    .pipe(gulp.dest(PATHS.images.dest))
+}
+
+function watch() {
+    const server = plugins.liveServer.static('dist');
+    server.start();
+
+    gulp.watch(['dist/**'])
+        .on('all', (evt, path) => server.notify({ path }))
+    gulp.watch([PATHS.content.files, PATHS.layouts.files, PATHS.partials.files], content)
+        .on('all', changeEvent('Content'))
+    gulp.watch([PATHS.styles.sass.files, PATHS.styles.css.files], styles)
+        .on('all', changeEvent('Styles'))
+    gulp.watch(PATHS.scripts.src, scripts)
+        .on('all', changeEvent('Scripts'))
+    gulp.watch(PATHS.images.src, images)
+        .on('all', changeEvent('Images'))
+}
+
+const all = gulp.parallel(content, styles, scripts, images)
+const serve = gulp.series(all, watch)
+
+module.exports = { all, serve, default: all }
